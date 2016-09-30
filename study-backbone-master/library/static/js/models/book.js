7a220e407242704257cf3e60042ab9e15b0e1d5f
Qboooogle <1432500814@qq.com>
2016-09-30 12:19:00 +0800

    backbone model 模型位描述数据组成

--- study-backbone-master/library/static/js/models/book.js
+++ study-backbone-master/library/static/js/models/book.js
@@ -1,3 +1,5 @@
+//模型用以描述数据组成，与服务器端同步（获取数据，更新数据，
+//删除数据），同样还可以使用浏览器端的localStorage将数据储存在前端。
 var app = app || {};
 
 app.BookModel= Backbone.Model.extend({
@@ -7,5 +9,13 @@ app.BookModel= Backbone.Model.extend({
        author:'佚名',
        releaseDate:'未知',
        keywords:'未知'
+   }
+   //还可以添加校验
+   //validate：function(attrs) {
+   //  if (attrs.name.length == 0) {
+   //      return "name can not be empty!"
+   //     }
+   //}
+   }
    }
 });
