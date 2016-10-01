b1810ae2b99117d49de21d2955729cde82830dfe
Qboooogle <1432500814@qq.com>
2016-10-01 18:28:40 +0800

    app.js 修复

--- study-backbone-master/library/static/js/app.js
+++ study-backbone-master/library/static/js/app.js
@@ -4,10 +4,10 @@ $(function(){
 
     //books数据库
     var books = [
-        {title:'第一本书',author:'湿哒哒',releaseDate:'2007','keywords':'sdffd'},
-        {title:'第er本书',author:'湿哒哒',releaseDate:'2007','keywords':'sdffd'},
-        {title:'第san本书',author:'湿哒哒',releaseDate:'2007','keywords':'sdffd'},
-        {title:'第si本书',author:'湿哒哒',releaseDate:'2007','keywords':'sdffd'}
+        {title:'第一本书',author:'Qboooogle',releaseDate:'2016','keywords':'sdffd'},
+        {title:'第er本书',author:'Qboooogle',releaseDate:'2016','keywords':'sdffd'},
+        {title:'第san本书',author:'Qboooogle',releaseDate:'2016','keywords':'sdffd'},
+        {title:'第si本书',author:'Qboooogle',releaseDate:'2016','keywords':'sdffd'}
     ];
 
     new app.LibraryView(books);
